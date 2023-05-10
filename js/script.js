9429851b9a8769f525bd067583dc325a72fdbc0a
Toluwalope-coast <rodneytoluwalope@gmail.com>
2023-05-10 20:10:26 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
-  
+}
+
+window.addEventListener("scroll", reveal);
+
 
 var slideIndex = 0;
 showSlides();
@@ -26,14 +26,26 @@ function showSlides() {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+const navToggle = document.querySelector('.mobile-nav-toggle')
+const nav = document.querySelector('.primary-navigation')
+
+navToggle.addEventListener('click', () => {
+  if (nav.hasAttribute('data-visible')) {
+    navToggle.setAttribute("aria-expanded", false)
+  } else {
+    navToggle.setAttribute("aria-expanded", true)
+  }
+  nav.toggleAttribute('data-visible')
+})
