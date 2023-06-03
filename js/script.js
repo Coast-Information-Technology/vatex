cddf5ca84defe2e02cb7de66b66ea6caa528bf0e
Toluwalope-coast <rodneytoluwalope@gmail.com>
2023-06-03 16:45:29 +0100

    landing page completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+AOS.init({
+  offset: 80,
+  duration: 900,
+});
+
+const mediaScroller = document.querySelector(".media-scroller");
+const leftController = document.querySelector(".left");
+const rightController = document.querySelector(".right");
+const mediaElements = document.querySelector(".media-element");
+
+const mediaElementsTotal = mediaElements.classList.length;
+let counter = 0;
+
+const imageSlider = () => {
+  leftController.addEventListener("click", () => {
+    console.log(`This is the total media element ${mediaElementsTotal}`);
+    console.log(`before clicking is ${counter}`);
+    counter = counter + 35;
+    mediaScroller.style.marginLeft = `${counter}%`;
+    c;
+    console.log("Left is clicked");
+    console.log(`after clicking is ${counter}`);
+  });
+  rightController.addEventListener("click", () => {
+    console.log(`This is the total media element ${mediaElementsTotal}`);
+    console.log(`before clicking is ${counter}`);
+    counter = counter - 35;
+    mediaScroller.style.marginLeft = `${counter}%`;
+    console.log("right is clicked");
+    console.log(`after clicking is ${counter}`);
+  });
+};
+imageSlider();
+
 
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
@@ -65,3 +99,69 @@ if (mobileScreen <= 768) {
     subMenu.toggleAttribute('data-visible')
   })
 }
+
+
+
+(function (w, d) {
+  !(function (a, b, c, d) {
+    a[c] = a[c] || {};
+    a[c].executed = [];
+    a.zaraz = { deferred: [], listeners: [] };
+    a.zaraz.q = [];
+    a.zaraz._f = function (e) {
+      return function () {
+        var f = Array.prototype.slice.call(arguments);
+        a.zaraz.q.push({ m: e, a: f });
+      };
+    };
+    for (const g of ["track", "set", "debug"]) a.zaraz[g] = a.zaraz._f(g);
+    a.zaraz.init = () => {
+      var h = b.getElementsByTagName(d)[0],
+        i = b.createElement(d),
+        j = b.getElementsByTagName("title")[0];
+      j && (a[c].t = b.getElementsByTagName("title")[0].text);
+      a[c].x = Math.random();
+      a[c].w = a.screen.width;
+      a[c].h = a.screen.height;
+      a[c].j = a.innerHeight;
+      a[c].e = a.innerWidth;
+      a[c].l = a.location.href;
+      a[c].r = b.referrer;
+      a[c].k = a.screen.colorDepth;
+      a[c].n = b.characterSet;
+      a[c].o = new Date().getTimezoneOffset();
+      if (a.dataLayer)
+        for (const n of Object.entries(
+          Object.entries(dataLayer).reduce(
+            (o, p) => ({ ...o[1], ...p[1] }),
+            {}
+          )
+        ))
+          zaraz.set(n[0], n[1], { scope: "page" });
+      a[c].q = [];
+      for (; a.zaraz.q.length;) {
+        const q = a.zaraz.q.shift();
+        a[c].q.push(q);
+      }
+      i.defer = !0;
+      for (const r of [localStorage, sessionStorage])
+        Object.keys(r || {})
+          .filter((t) => t.startsWith("_zaraz_"))
+          .forEach((s) => {
+            try {
+              a[c]["z_" + s.slice(7)] = JSON.parse(r.getItem(s));
+            } catch {
+              a[c]["z_" + s.slice(7)] = r.getItem(s);
+            }
+          });
+      i.referrerPolicy = "origin";
+      i.src =
+        "../../cdn-cgi/zaraz/sd0d9.js?z=" +
+        btoa(encodeURIComponent(JSON.stringify(a[c])));
+      h.parentNode.insertBefore(i, h);
+    };
+    ["complete", "interactive"].includes(b.readyState)
+      ? zaraz.init()
+      : a.addEventListener("DOMContentLoaded", zaraz.init);
+  })(w, d, "zarazData", "script");
+})(window, document);
